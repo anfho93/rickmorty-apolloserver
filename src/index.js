6023fb48da598b5289c3c0df029d4f3bbfd591b9
Andres Herrera <andres.h@smartenit.com>
2020-03-04 20:36:51 -0500

    Initial commit with graphql fetch data from the rick & morty REST api

---
+++ src/index.js
@@ -0,0 +1,148 @@
+const fetch = require("node-fetch");
+const { ApolloServer, gql } = require("apollo-server");
+
+// This is a (sample) collection of books we'll be able to query
+// the GraphQL server for.  A more complete example might fetch
+// from an existing data source like a REST API or database.
+const books = [
+  {
+    title: "Harry Potter and the Chamber of Secrets",
+    author: "J.K. Rowling"
+  },
+  {
+    title: "Jurassic Park",
+    author: "Michael Crichton"
+  }
+];
+
+// mock data for characters
+const characters = [
+  {
+    name: "Rick Sanchez",
+    id: 1,
+    status: "Alive",
+    episodes: [
+      "https://rickandmortyapi.com/api/episode/1",
+      "https://rickandmortyapi.com/api/episode/2"
+    ]
+  },
+  {
+    name: "Morty Smith",
+    id: 2,
+    status: "Alive",
+    episodes: [
+      "https://rickandmortyapi.com/api/episode/1",
+      "https://rickandmortyapi.com/api/episode/3"
+    ]
+  }
+];
+
+
+// mock data for episodes
+const episodes = [
+  {
+    name: "Pilot",
+    id: 1
+  },
+  {
+    name: "Lawnmower Dog",
+    id: 2
+  }
+];
+// Type definitions define the "shape" of your data and specify
+// which ways the data can be fetched from the GraphQL server.
+const typeDefs = gql`
+  # Comments in GraphQL are defined with the hash (#) symbol.
+
+  type Book {
+    title: String
+    author: String
+  }
+
+  enum CharacterStatus {
+    Alive
+    Dead
+    unknown
+  }
+
+  # This "Character" type can be used in other type declarations.
+  type Character {
+    id: ID
+    name: String
+    image: String
+    status: CharacterStatus
+    episode: [String]
+  }
+
+  type Episode {
+    id: ID
+    name: String
+  }
+
+  # The "Query" type is the root of all GraphQL queries.
+  # (A "Mutation" type will be covered later on.)
+  type Query {
+    books: [Book]
+    characters: [Character]
+    character(id: ID!): Character
+    episode(id: ID!): Episode
+    episodes: [Episode]
+  }
+`;
+
+
+// Resolvers define the technique for fetching the types in the
+// schema.  We'll retrieve books from the "books" array above.
+const resolvers = {
+  Query: {
+    books: () => books,
+    characters: () => fetchCharacters(),
+    character: (parent, args) => {
+      const {id} = args;
+      return fetchCharacterById(id)
+    },
+    episode: (parent, args) => {
+      const {id} = args;
+      return fetchEpisodeById(id)
+    },
+    episodes: () => fetchEpisodes()
+  }
+};
+
+// In the most basic sense, the ApolloServer can be started
+// by passing type definitions (typeDefs) and the resolvers
+// responsible for fetching the data for those types.
+const server = new ApolloServer({ typeDefs, resolvers });
+
+// This `listen` method launches a web-server.  Existing apps
+// can utilize middleware options, which we'll discuss later.
+server.listen().then(({ url }) => {
+  console.log(`🚀  Server ready at ${url}`);
+});
+
+function fetchEpisodes() {
+  // More info about the fetch function? https://github.com/bitinn/node-fetch#json
+  return fetch("https://rickandmortyapi.com/api/episode/")
+    .then(res => res.json())
+    .then(json => json.results);
+}
+
+function fetchEpisodeById(id) {
+  return fetch("https://rickandmortyapi.com/api/episode/" + id)
+    .then(res => res.json())
+    .then(json => json);
+}
+
+function fetchCharacters() {
+  // More info about the fetch function? https://github.com/bitinn/node-fetch#json
+  return fetch("https://rickandmortyapi.com/api/character/")
+    .then(res => res.json())
+    .then(json => json.results);
+}
+
+function fetchCharacterById(id) {
+  // More info about the fetch function? https://github.com/bitinn/node-fetch#json
+  return fetch("https://rickandmortyapi.com/api/character/" + id)
+    .then(res => res.json())
+    .then(json => json);
+}
